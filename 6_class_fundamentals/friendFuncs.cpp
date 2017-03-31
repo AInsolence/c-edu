6de16b00d44dda3ba3a76a1c3a165583da59961c
AInsolence <antonpythondev@gmail.com>
2017-03-31 22:59:57 +0600

    friend func example;"

--- 6_class_fundamentals/friendFuncs.cpp
+++ 6_class_fundamentals/friendFuncs.cpp
@@ -11,6 +11,7 @@ public:
 	char name[80];
 	int id;
 	char description[80];
+	void get_AnotherObj_status(AnotherSample *obj_ptr);
 	friend void show_all(sample *object);
 	friend void get_password(sample * object, AnotherSample * object2);
 	sample(int num);
@@ -37,7 +38,9 @@ class AnotherSample
 	int my_secret;
 	int i;
 	char name[30];
+	int status;
 public:
+	friend void sample :: get_AnotherObj_status(AnotherSample *obj_ptr);
 	friend void get_password(sample * object, AnotherSample * object2);
 	AnotherSample(int i);
 	~AnotherSample();
@@ -48,6 +51,7 @@ AnotherSample :: AnotherSample(int i)
 {
 	my_secret = (i-32)/2;
 	i = 2;
+	status = 22;
 	strcpy(name, "HAHAa!");
 }
 
@@ -56,12 +60,14 @@ AnotherSample :: ~AnotherSample()
 	return;
 }
 
-void show_all(sample *object)
+// Friend for sample class
+void show_all(sample *object) 
 {
 	cout << object -> name << " password is " << object -> password << endl;
 	return;
 }
 
+// Friend for both classes
 void get_password(sample * object, AnotherSample * object2)
 {
 	cout << "First pass: " << object -> password << endl;
@@ -69,6 +75,13 @@ void get_password(sample * object, AnotherSample * object2)
 	return;
 }
 
+// Member of sample class & Friend for AnotherSample class
+void sample :: get_AnotherObj_status(AnotherSample *obj_ptr) 
+{
+	cout << "Sample Object status is " << id << endl;
+	cout << "AnotherSample Object status is " << obj_ptr -> status << endl;
+	return;
+}
 
 int main(int argc, char const *argv[])
 {
@@ -77,11 +90,17 @@ int main(int argc, char const *argv[])
 	sample y(20), *y_ptr;
 	x_ptr = &x;
 	y_ptr = &y;
+
+	show_all(x_ptr); // Friend for sample class
+	show_all(y_ptr);
+
 	AnotherSample z(75), * z_ptr;
 	z_ptr = &z;
-	show_all(x_ptr);
-	show_all(y_ptr);
-	get_password(x_ptr, z_ptr);
+
+	get_password(x_ptr, z_ptr); // Friend for both classes
+
+	x.get_AnotherObj_status(z_ptr); // Member of sample class & Friend for AnotherSample class
+
 	cout << "Program is well done!" << endl;
 	return 0;
 }
