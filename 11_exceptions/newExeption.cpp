4785142a27decf06c4fbee02fe9dbf77e4af68df
AInsolence <antonpythondev@gmail.com>
2017-04-28 21:49:51 +0600

    new operator exceptionl

--- 11_exceptions/newExeption.cpp
+++ 11_exceptions/newExeption.cpp
@@ -2,3 +2,23 @@
 #include <new>
 
 using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int *p, i;
+	try{
+		p = new int[32];
+	}
+
+	catch (bad_alloc xa){
+		cout << "Memory is not allocated!\n";
+		return 1;
+	}
+
+	for (i = 0; i < 32; i++) p[i] = i;
+	for (i = 0; i < 32; i++) cout << p[i] << ' ';
+
+	delete [] p;
+
+	return 0;
+}
