f6bf9ae43a352a67c8477584611c41a6dcddeead
AInsolence <antonpythondev@gmail.com>
2017-06-30 12:01:33 +0600

    example + ptr to class member func

--- 14_Scopes&OtherThemes/explicitConstructor&constInit.cpp
+++ 14_Scopes&OtherThemes/explicitConstructor&constInit.cpp
@@ -17,4 +17,33 @@ public:
 	~ConstMemberOwner(){}
 	const int NUMBER;
 	const int OLD;
+	void show() {cout << NUMBER << " " << OLD << endl;}
 };
+
+void show_info(int a)
+{
+	cout << "This is a programm number " << a << endl;
+	return;
+}
+
+int main(int argc, char const *argv[])
+{
+	void (*ptr)(int) = NULL;
+	ptr = show_info;
+	MyExp object(100);
+	// object = 2000; Does not work with explicit constructor
+
+	ConstMemberOwner obj (5, 3);
+	obj.show();
+
+	ptr(1);
+
+	//Pointer to class member function
+	typedef void (ConstMemberOwner::*memPtr)();
+
+	memPtr ptr_CM = &ConstMemberOwner::show;
+
+	(obj.*ptr_CM)();
+
+	return 0;
+}
