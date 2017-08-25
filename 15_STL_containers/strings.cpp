2b00f8db091a1e4cf16a91eabd26329ea2e58cb7
AInsolence <antonpythondev@gmail.com>
2017-08-25 12:54:40 +0600

    string methods add

---
+++ 15_STL_containers/strings.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <iterator>
+#include <algorithm>
+
+
+int main(int argc, char const *argv[])
+{
+	std::string first_name, second_name, full_name, part_str;
+	first_name = {'A', 'n', 't', 'o', 'n'};
+	second_name = "Insolence";
+	full_name = first_name + ' ';
+	full_name += second_name;
+	copy(full_name.begin(), full_name.end(),\
+			std::ostream_iterator<char>(std::cout));
+	part_str.assign(full_name.begin(), full_name.begin() + 3);
+	std::cout << std::endl << part_str << std::endl;
+	first_name.append(" ");
+	first_name.append(part_str);
+	std::cout << first_name << std::endl;
+	std::string *ptr_str;
+	ptr_str = new std::string;
+	*ptr_str = "XXXXX";
+	//NOTE!!! here we use INDEX as argument, not iterator!
+	first_name.insert(3, *ptr_str);
+	std::cout << first_name << std::endl;
+	delete ptr_str;
+	copy_if(full_name.begin(), full_name.end(),\
+			std::ostream_iterator<char>(std::cout),\
+				[](char ob){return ob != 'X';});
+
+	return 0;
+}
